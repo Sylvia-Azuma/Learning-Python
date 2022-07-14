10a5adbc0feab06dd4cf0a5a79b8952948483ee8
Sylvia-Azuma <sylviaazuma@gmail.com>
2022-07-14 17:52:29 +0000

    a commit

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+print("A Program To Display Your Year Of Birth")
+nameofperson = input("Enter your name: ")
+ageofperson = int(input("Enter your age: "))
+currentyear = 2022
+def sub():
+    return(current - ageofperson)
+print("Hello, " +nameofperson, "You were born in the year" ,sub())
